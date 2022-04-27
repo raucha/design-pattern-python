fd9b725a20e0589c5134d2031d12119274b43555
raucha <raucha@users.noreply.github.com>
2022-04-27 11:19:30 +0000

    init

---
+++ 19_State/classdef/StatePatternRobot.py
@@ -0,0 +1,15 @@
+
+class StatePatternRobot():
+    def __init__() -> None:
+        self.__state = None
+    
+    def change_state(new_state):
+        self.__state = new_state
+
+    def is_running():
+        self.__state.is_runnable()
+    
+    def show_display_msg():
+        self.__state.show_display_msgs()
+
+
